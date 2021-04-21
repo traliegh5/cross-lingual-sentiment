fixed accuracy and overall f1 calculation, removed nlproc

model.py
@@ -9,7 +9,6 @@ class Sentiment_Analysis_Model(nn.Module):
         
         '''
         self.device_type = device_type
-        #self.xlmr_model = XLMRobertaModel.from_pretrained('xlm-roberta-base')
         if (model_type == 'xlmr'):
             self.model = XLMRobertaModel.from_pretrained('xlm-roberta-base')
         else: 
@@ -34,11 +33,7 @@ class Sentiment_Analysis_Model(nn.Module):
         (_, pooler_output) = self.model(inputs,attention_mask=attention_mask).to_tuple()
         dropout_output = self.dropout(pooler_output)
         dense_output = self.dense(dropout_output)
-
-        if (self.num_classes == 2):
-            to_return = self.softmax(dense_output)
-        else: 
-            to_return = self.sig(dense_output)
+        to_return = self.sig(dense_output)
 
         return (dense_output, to_return)
 

preprocess.py
@@ -2,6 +2,8 @@ from transformers import XLMRobertaTokenizer
 from torch.utils.data import Dataset, DataLoader
 import torch
 from torch.nn.utils.rnn import pad_sequence
+import numpy as np
+
 def convert_to_binary(label):
     # anger:1, anticipation:2, disgust:3, fear:4, joy:5, sadness:6, surprise:7, trust:8, with neutral:0
     label=torch.as_tensor(label)
@@ -18,6 +20,14 @@ def convert_to_binary(label):
     else:
         val=None
     return val 
+
+def calculate_pos_weights(class_counts, num_examples):
+    pos_weights = np.ones_like(class_counts)
+    neg_counts = [num_examples-pos_count for pos_count in class_counts]
+    for i in range(len(class_counts)):
+        pos_weights[i] = neg_counts[i] / (class_counts[i] + 1e-5)
+
+    return torch.as_tensor(pos_weights, dtype=torch.float)
 
 class SentimentData(Dataset):
     def __init__(self, input_file, tokenizer, dataset_name, num_classes):
@@ -43,10 +53,6 @@ class SentimentData(Dataset):
         #       and make sure you pad your inputs.
 
         # Hint: remember to add start and pad to create inputs and labels
-        self.binary = False
-        if (dataset_name != "nlproc") & (num_classes == 2):
-            self.binary = True
-
         self.lengths=[]
         self.labels=[]
         self.tokenizer=tokenizer
@@ -59,7 +65,8 @@ class SentimentData(Dataset):
 
         self.tokens=[]
         self.texts=[]
-        # self.start=tokenizer.bos_token_id
+        multi_class_counts = [0] * 8
+        binary_class_counts = [0] * num_classes
 
         with open(input_file,'r') as f:
             
@@ -69,58 +76,43 @@ class SentimentData(Dataset):
                 if not line:
                     break
 
-                if (dataset_name == "nlproc"):
-                    self.labels.append(int(line[0]))
-                    text = line[1:]
-                    self.texts.append(text)
-                    # toke=self.tokenizer(text)['input_ids'][:(window_size)]
-                    toke=self.tokenizer(text)['input_ids']
-                    #toke.insert(0,self.cls_token)
-                    #toke.append(self.sep_token)
-                    self.tokens.append(torch.as_tensor(toke))
-                else:
-                    tabbed = line.split("\t")
-                    text = tabbed[0]
-                    line_labels = tabbed[1]
-                    # toke=self.tokenizer(text)['input_ids'][:(window_size)]
-                    toke=self.tokenizer(text)['input_ids']
-                    #toke.insert(0,self.cls_token)
-                    #toke.append(self.sep_token)
-                    num_emotions = 8
-                    prepped_label = [0] * num_emotions
+                tabbed = line.split("\t")
+                text = tabbed[0]
+                line_labels = tabbed[1]
+                toke=self.tokenizer(text)['input_ids']
+                num_emotions = 8
+                prepped_label = [0] * num_emotions
 
-                    for i in range(num_emotions):
-                        if str(i+1) in line_labels:
-                            prepped_label[i] = 1
-                    if self.binary:
+                for i in range(num_emotions):
+                    if str(i+1) in line_labels:
+                        prepped_label[i] = 1
+                        multi_class_counts[i] += 1
 
-                        sentiment=convert_to_binary(prepped_label)
-                        if sentiment==None:
-                            #print("Throw this line out")
-                            #print(prepped_label)
-                            continue
-                        else:
-                            self.labels.append(sentiment)
+                if (num_classes == 1):
+                    sentiment=convert_to_binary(prepped_label)
+                    if sentiment==None:
+                        #print("Throw this line out")
+                        #print(prepped_label)
+                        continue
                     else:
-                        self.labels.append(prepped_label)
-                    self.texts.append(text)
-                    
-                    self.tokens.append(torch.as_tensor(toke))
-                # cent.insert(0,'START')
-                # token=self.tokenizer(line)['input_ids']
-                # token=token[:self.window_size]
-                # token.insert(0,self.start)
-                # token.append(self.start)
+                        self.labels.append([sentiment])
+                        binary_class_counts[0] += sentiment
+                else:
+                    self.labels.append(prepped_label)
+                self.texts.append(text)
                 
-                # self.tense.append(torch.as_tensor(token))
-                # temp=torch.ones(self.window_size+2)
-                # temp[len(token):]=0.0
-                # self.masks.append(temp)
-                
+                self.tokens.append(torch.as_tensor(toke))
                 self.lengths.append(len(toke))
        
         self.labels=torch.as_tensor(self.labels)
         self.tokens=pad_sequence(self.tokens,batch_first=True,padding_value=self.pad_token)
+        num_examples  = len(self.tokens)
+        if (num_classes == 1):
+            class_counts = binary_class_counts
+        else: 
+            class_counts = multi_class_counts
+
+        self.pos_weights = calculate_pos_weights(class_counts, num_examples)
         # print("Size of tokens: " + str(self.tokens.size()))
         # print("Size of labels: " + str(self.labels.size()))
         # print("Size of lengths: " + str(len(self.lengths)))
@@ -138,9 +130,6 @@ class SentimentData(Dataset):
         len should return a the length of the dataset
         :return: an integer length of the dataset
         """
-        # TODO: Override method to return length of dataset
-        
-        # return self.sentences.shape[0]
         return self.labels.shape[0]
 
     def __getitem__(self, idx):
@@ -151,9 +140,6 @@ class SentimentData(Dataset):
         :param idx: the index for retrieval
         :return: tuple or dictionary of the data
         """
-        # TODO: Override method to return the items in dataset
-       
-        #item={"input":self.tokens[idx,:],"label":self.labels[idx],"pad_token":self.pad_token}
         item={"input":self.tokens[idx,:],"label":self.labels[idx],"lengths":self.lengths[idx]}
         return (self.tokens[idx], self.labels[idx], self.lengths[idx])
 

prev_final_with_crossentropy.py
@@ -30,8 +30,10 @@ def train(model, train_loader, optimizer,scheduler,experiment, num_classes, pos_
     # Loss 
         
     torch.cuda.empty_cache()
-    
-    loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+    if (num_classes==2):
+        loss_fn = nn.CrossEntropyLoss(ignore_index=pad_id)
+    else:
+        loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 
     total_loss = 0
     losses = []
@@ -39,7 +41,6 @@ def train(model, train_loader, optimizer,scheduler,experiment, num_classes, pos_
     total_word_count = 0
     correct_predictions = 0
     num_examples = 0
-    total_batches = 0
 
     model = model.train()
 
@@ -55,7 +56,6 @@ def train(model, train_loader, optimizer,scheduler,experiment, num_classes, pos_
                 lengths = lengths.to(device)
 
                 batch_num += 1
-                total_batches += 1
 
                 optimizer.zero_grad()
                 (logits, probs) = model(inputs, lengths)
@@ -65,7 +65,10 @@ def train(model, train_loader, optimizer,scheduler,experiment, num_classes, pos_
                 #print(round_probs)
                 # print(labels[:10])
 
-                labels_for_loss = labels.type_as(logits)
+                if (num_classes == 2):
+                    labels_for_loss = labels
+                else:
+                    labels_for_loss = labels.type_as(logits)
 
                 loss = loss_fn(logits, labels_for_loss)
                 losses.append(loss.item())
@@ -80,23 +83,22 @@ def train(model, train_loader, optimizer,scheduler,experiment, num_classes, pos_
                 word_count = sum(lengths)
                 total_loss += (loss * word_count)
                 total_word_count += word_count
-
-                if (num_classes == 1):
-                    f1 = f1_score(labels.cpu().data.numpy(), round_probs, average='macro',zero_division=1)
+                indices = torch.max(probs, 1)[1].cpu().data.numpy()
+                if (num_classes == 2):
+                    f1 = f1_score(labels.cpu().data.numpy(), indices, average='binary',zero_division=1)
+                    num_correct = np.sum(indices == labels.cpu().data.numpy())
+                    accuracy_out_of = num_in_batch
+                    print("Num correct: " + str(num_correct))
+                    print(indices)
+                    print(labels)
                 else:
                     f1 = f1_score(labels.cpu().data.numpy(), round_probs, average='macro',zero_division=1)
-
-                # print("Round probs")
-                # print(round_probs)
-                # print("Labels")
-                # print(labels)
-                num_correct = np.sum((round_probs == labels.cpu().data.numpy()).all(1))
-                #num_correct = np.sum(round_probs == labels.cpu().data.numpy())
-                accuracy_out_of = num_in_batch
-                print("Num correct: " + str(num_correct))
-                print("F1: " + str(f1))
-                # print(round_probs)
-                # print(labels.cpu().data.numpy())
+                    num_correct = np.sum((round_probs == labels.cpu().data.numpy()).all(1))
+                    #num_correct = np.sum(round_probs == labels.cpu().data.numpy())
+                    accuracy_out_of = num_in_batch
+                    # print("Num correct: " + str(num_correct))
+                    # print(round_probs)
+                    # print(labels.cpu().data.numpy())
 
                 total_f1 += f1
                 correct_predictions += num_correct
@@ -117,7 +119,7 @@ def train(model, train_loader, optimizer,scheduler,experiment, num_classes, pos_
         accuracy = correct_predictions / num_examples
         perplexity = np.exp(mean_loss)
         #perplexity = torch.exp(mean_loss).detach()
-        overall_f1 = total_f1/total_batches
+        overall_f1 = total_f1/batch_num
 
         print("perplexity:", perplexity)
         print("F1:", overall_f1)
@@ -155,35 +157,32 @@ def test(model, test_loader, experiment, num_classes, pos_weight, hyperparams, p
 
                 batch_num += 1
 
-                optimizer.zero_grad()
                 (logits, probs) = model(inputs, lengths)
 
-                #labels = labels.type_as(logits)
-                round_probs = np.round(probs.cpu().data.numpy())
-                #print(round_probs)
-                # print(labels[:10])
-
-                labels_for_loss = labels.type_as(logits)
+                if (num_classes == 2):
+                    labels_for_loss = labels
+                else:
+                    labels_for_loss = labels.type_as(logits)
 
                 loss = loss_fn(logits, labels_for_loss)
                 losses.append(loss.item())
-
+                round_probs = np.round(probs.cpu().data.numpy())
+                
                 word_count = sum(lengths)
                 total_loss += (loss * word_count)
                 total_word_count += word_count
-
-                if (num_classes == 1):
-                    f1 = f1_score(labels.cpu().data.numpy(), round_probs, average='macro',zero_division=1)
+                indices = torch.max(probs, 1)[1].cpu().data.numpy()
+                
+                if (num_classes == 2):
+                    f1 = f1_score(labels.cpu().data.numpy(), indices, average='binary',zero_division=1)
+                    num_correct = np.sum(indices == labels.cpu().data.numpy())
+                    accuracy_out_of = num_in_batch
                 else:
                     f1 = f1_score(labels.cpu().data.numpy(), round_probs, average='macro',zero_division=1)
-
-                num_correct = np.sum((round_probs == labels.cpu().data.numpy()).all(1))
-                #num_correct = np.sum(round_probs == labels.cpu().data.numpy())
-                accuracy_out_of = num_in_batch
-                print("Num correct: " + str(num_correct))
-                print("F1: " + str(f1))
-                # print(round_probs)
-                # print(labels.cpu().data.numpy())
+                    #num_correct = np.sum(round_probs == labels.cpu().data.numpy())
+                    #accuracy_out_of = num_in_batch*8
+                    num_correct = np.sum((round_probs == labels.cpu().data.numpy()).all(1))
+                    accuracy_out_of = num_in_batch
 
                 total_f1 += f1
                 correct_predictions += num_correct
@@ -193,16 +192,20 @@ def test(model, test_loader, experiment, num_classes, pos_weight, hyperparams, p
 
         #mean_loss = total_loss / total_word_count
         mean_loss = np.mean(losses)
-        print("Mean loss: " + str(mean_loss))
-        accuracy = correct_predictions / num_examples
-        perplexity = np.exp(mean_loss)
         #perplexity = torch.exp(mean_loss).detach()
+        perplexity = np.exp(mean_loss)
+        overall_f1 = total_f1/batch_num
+        accuracy = correct_predictions / num_examples
+
+        mean_loss = total_loss / total_word_count
+        perplexity = torch.exp(mean_loss).detach()
+        # overall_f1 = total_f1/num_in_batch
         overall_f1 = total_f1/batch_num
 
         print("perplexity:", perplexity)
         print("F1:", overall_f1)
         print("Accuracy:", accuracy)
-        experiment.log_metric("perplexity", perplexity)
+        experiment.log_metric("perplexity", perplexity.cpu())
         experiment.log_metric("accuracy", accuracy)
         experiment.log_metric("F1", overall_f1)
 
@@ -213,7 +216,7 @@ if __name__ == "__main__":
     parser.add_argument("-lang", "--language", type=str, default="related",
                         help="related or diff")
     parser.add_argument("-n", "--num_classes", type=str, default="8",
-                        help="1 or 8")
+                        help="2 or 8")
     parser.add_argument("-l", "--load", action="store_true",
                         help="load model.pt")
     parser.add_argument("-s", "--save", action="store_true",
@@ -231,21 +234,34 @@ if __name__ == "__main__":
 
     dataset_name = "xed"
 
+    binary = True
+
     if (model_type == "xlmr"):
         tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base')
         
-        if (train_lang == 'related'):
-            train_file = "XED/en-annotated.tsv"
-        else: 
-            train_file = "XED/fi-annotated.tsv"
+        if (dataset_name == "nlproc"):
+            if (train_lang == 'related'):
+                train_file = "Data/it/train.tsv"
+            else: 
+                train_file = "Data/jp/train.tsv"
 
-        test_file = "XED/de-projections.tsv"
-            
+            test_file = "Data/de/test.tsv"
+        else: 
+            if (train_lang == 'related'):
+                train_file = "XED/en-annotated.tsv"
+            else: 
+                train_file = "XED/fi-annotated.tsv"
+
+            test_file = "XED/de-projections.tsv"
     else:
         tokenizer = AutoTokenizer.from_pretrained("bert-base-german-cased")
 
-        train_file = "XED/de-projections.tsv"
-        test_file = "XED/de-projections.tsv"    
+        if (dataset_name == "nlproc"):
+            train_file = "Data/de/train.tsv"
+            test_file = "Data/de/test.tsv"
+        else: 
+            train_file = "XED/de-projections.tsv"
+            test_file = "XED/de-projections.tsv"
 
     vocab_size = len(tokenizer)
     model = Sentiment_Analysis_Model(vocab_size, model_type, num_classes, device_type).to(device)
@@ -255,8 +271,8 @@ if __name__ == "__main__":
     
     train_dataset = SentimentData(train_file, tokenizer, dataset_name, num_classes)
     test_dataset = SentimentData(test_file, tokenizer, dataset_name, num_classes)
-    train_pos_weights = train_dataset.pos_weights.to(device)
-    test_pos_weights = test_dataset.pos_weights.to(device)
+    train_pos_weights = train_dataset.pos_weights
+    test_pos_weights = test_dataset.pos_weights
     pad_token = train_dataset.pad_token
 
     # if (dataset_name!= "nlproc") and (model_type!="xlmr"):